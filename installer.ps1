e6cdeff56a07cb786714337b128380993c2a52f0
Erica Warren <erica.warren@macmillan.com>
2017-02-17 00:37:41 -0500

    Fixing so-called smart quotes that got in there somehow

--- installer.ps1
+++ installer.ps1
@@ -23,9 +23,9 @@ function ConvertFrom-Json20([object] $item){
 
 # Check if passed installType variable, if not exit
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] “Administrator”))
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-    Write-Warning “You do not have Administrator rights to run this script.`nPlease re-run this script as an Administrator.”
+    Write-Warning "You do not have Administrator rights to run this script.`nPlease re-run this script as an Administrator."
     Break
 }
 
